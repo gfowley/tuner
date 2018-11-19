4660fec02463f88e55f80e441e37cd18f383afa6
Gerard Fowley <gerard.fowley@iqeo.net>
2018-11-19 08:50:06 -0500

    more fft graph progress

--- app/vue_freq_graph.rb
+++ app/vue_freq_graph.rb
@@ -5,9 +5,9 @@ class VueFreqGraph < Vue
   name     'freq-graph'
   template '#freq-graph-template'
 
-  props    :freq_data, :rate
+  props     :freq, :freq_data, :rate
 
-  computed :points, :grid
+  computed :points, :grid, :indicator
 
   GRAPH_FFT_WINDOW = 8 # fraction (divisor) of FFT to display
   GRAPH_WIDTH = 1024 # TODO: what should this be (calculate from FFT size ?)
@@ -20,18 +20,29 @@ class VueFreqGraph < Vue
   end
 
   def grid
-    return [] if rate == 0 # avoid -Infinity from log2(0)
+    return [] if rate == 0 # avoid Infinity from log2(0)
     # mark octaves between lower and upper frequency limits
     lower_freq_limit = 55 # A(1)
-    upper_freq_limit = rate / 2 / GRAPH_FFT_WINDOW
     lower_octaves = Math.log2(lower_freq_limit).floor
     upper_octaves = Math.log2(upper_freq_limit).floor
     (upper_octaves-lower_octaves).times.map do |octave|
       {
-        freq: ( freq = ( 55 * 2**octave ).to_i ), # avoid strange 55/1 fraction in js conversion?
-        x:    ( GRAPH_WIDTH * ( freq / upper_freq_limit ) ).round
+        freq: ( oct_freq = ( 55 * 2**octave ).to_i ), # avoid strange 55/1 fraction in js conversion?
+        x:    ( GRAPH_WIDTH * ( oct_freq / upper_freq_limit ) ).round
       }.to_n
     end
+  end
+
+  def indicator
+    return {} if rate == 0 # avoid Infinity from rate/upper_freq_limit
+    {
+      freq: freq.round,
+      x:    ( GRAPH_WIDTH * ( freq / upper_freq_limit ) ).round
+    }.to_n
+  end
+
+  def upper_freq_limit
+    rate / 2 / GRAPH_FFT_WINDOW
   end
 
 end 
